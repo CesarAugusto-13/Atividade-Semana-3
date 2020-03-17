6cce3ffaa776e6c8ee485c16b3a482fcfbbfb4e4
Cesar Dias <cesarvdias@hotmail.com>
2020-03-17 14:14:42 -0300

    Exercicio2

--- AreaDoRetangulo.java
+++ AreaDoRetangulo.java
@@ -4,13 +4,13 @@ public class AreaDoRetangulo
    public static void main (String [] args)
    {
    
-   double altura = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura do ret�ngulo"));
+   double altura = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura do retângulo"));
    
-   double largura = Double.parseDouble(JOptionPane.showInputDialog("Digite a largura do ret�ngulo"));
+   double largura = Double.parseDouble(JOptionPane.showInputDialog("Digite a largura do retângulo"));
    
    double area = altura * largura;
    
-   JOptionPane.showMessageDialog(null, "A area do ret�ngulo �: " + area);
+   JOptionPane.showMessageDialog(null, "A area do retângulo é: " + area);
    
    }
-}   
+}  
